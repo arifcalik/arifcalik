d61d6eb3e97dafb77a04d42d39b2f6035db4e488
arifcalik <arifcalik@gmail.com>
2023-11-02 15:22:50 +0100

    divide each use case into a single test for better debug

--- Nov01_python_intro2/test_unit_intro2.py
+++ Nov01_python_intro2/test_unit_intro2.py
@@ -11,49 +11,56 @@ class TestIntro2(unittest.TestCase):
     def tearDown(self) -> None:
         return super().tearDown()
     
-    def test_arrayCheck(self):
+    def test_empty_list_arrayCheck(self):
         #empty list - default in setup!
         result = arrayCheck(self.testList)
         #False is expected result
         self.assertEqual(result, False) 
 
+    def test_sample_list_arrayCheck(self):
         self.testList = [1, 1, 2, 0, 1, 3, 2, 1, 2, 1, 2, 4, 3, 1, 2,]
         result = arrayCheck(self.testList)
         self.assertEqual(result, False)
   
+    def test_sample_list_with_assertTrue_arrayCheck(self):
         #assertTrue
         self.testList = [3, 2, 1, 2, 3]
         result = arrayCheck(self.testList)
         #False is expected result
         self.assertTrue(result)               
     
-    def test_stringBits(self):
+    def test_empty_string_stringBits(self):
         #empty testString! - default
         result = stringBits(self.testString)
         self.assertEqual(result, '')
 
+    def test_sample_string_stringBits(self):
         self.testString = 'Wwworldlldde'
         result = stringBits(self.testString)
         self.assertEqual(result, 'Wwrdld')
         
 
-    def test_doubleChar(self):
+    def test_sample_string_doubleChar(self):
         self.testString = 'Hi-There'
         result = doubleChar(self.testString)
         self.assertEqual(result, 'HHii--TThheerree')
+
+    def test_empty_string_doubleChar(self):        
         # how about sending empty string
         self.testString = ''
         result = doubleChar(self.testString)
         self.assertEqual(result, '')        
 
-    def test_count_evens(self):
-        self.testString = [-1, 1, 7, 3, -123, 23, 67, 7,]
-        result = count_evens(self.testString)
+    def test_sample_list_count_evens(self):
+        self.testList = [-1, 1, 7, 3, -123, 23, 67, 7,]
+        result = count_evens(self.testList)
         self.assertEqual(result, 0)
+
+    def test_empty_list_count_evens(self):        
         '''
         how about sending empty list!
         '''
-        self.testString = []
+        self.testList = []
         result = 0  #as expected
         self.assertEqual(result, 0)
 
