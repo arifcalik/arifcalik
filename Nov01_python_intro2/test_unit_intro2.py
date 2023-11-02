9564c5a653fd07a9f43dbd51fdc2a9c88e75e1f6
arifcalik <arifcalik@gmail.com>
2023-11-02 12:16:19 +0100

    Setup and teardown, skip added

--- Nov01_python_intro2/test_unit_intro2.py
+++ Nov01_python_intro2/test_unit_intro2.py
@@ -2,44 +2,62 @@ import unittest
 from python_intro2 import arrayCheck, stringBits, doubleChar, count_evens
 
 class TestIntro2(unittest.TestCase):
+    def setUp(self) -> None:
+        self.testList = []
+        self.testString = '' 
+        return super().setUp()
+    
+    #even though not funtional here better to have for future use
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
     def test_arrayCheck(self):
-        testList1 = [1, 1, 2, 0, 1, 3, 2, 1, 2, 1, 2, 4, 3, 1, 2,]
-        result = arrayCheck(testList1)
-        self.assertEqual(result, False)
-        #empty list
-        testList1 = []
-        result = arrayCheck(testList1)
+        #empty list - default in setup!
+        result = arrayCheck(self.testList)
         #False is expected result
-        self.assertEqual(result, False)        
+        self.assertEqual(result, False) 
+
+        self.testList = [1, 1, 2, 0, 1, 3, 2, 1, 2, 1, 2, 4, 3, 1, 2,]
+        result = arrayCheck(self.testList)
+        self.assertEqual(result, False)
+  
+        #assertTrue
+        self.testList = [3, 2, 1, 2, 3]
+        result = arrayCheck(self.testList)
+        #False is expected result
+        self.assertTrue(result)               
     
     def test_stringBits(self):
-        input = 'Wwworldlldde'
-        result = stringBits(input)
+        #empty testString! - default
+        result = stringBits(self.testString)
+        self.assertEqual(result, '')
+
+        self.testString = 'Wwworldlldde'
+        result = stringBits(self.testString)
         self.assertEqual(result, 'Wwrdld')
-        #empty input!
-        input = ''
-        result = stringBits(input)
-        self.assertEqual(result, '')        
+        
 
     def test_doubleChar(self):
-        input = 'Hi-There'
-        result = doubleChar(input)
+        self.testString = 'Hi-There'
+        result = doubleChar(self.testString)
         self.assertEqual(result, 'HHii--TThheerree')
         # how about sending empty string
-        input = ''
-        result = doubleChar(input)
+        self.testString = ''
+        result = doubleChar(self.testString)
         self.assertEqual(result, '')        
 
     def test_count_evens(self):
-        input = [-1, 1, 7, 3, -123, 23, 67, 7,]
-        result = count_evens(input)
+        self.testString = [-1, 1, 7, 3, -123, 23, 67, 7,]
+        result = count_evens(self.testString)
         self.assertEqual(result, 0)
         '''
         how about sending empty list!
         '''
-        input = []
+        self.testString = []
         result = 0  #as expected
         self.assertEqual(result, 0)
-        
 
+    @unittest.skip("In progress") 
+    def test_skip(self):
+        pass
 
