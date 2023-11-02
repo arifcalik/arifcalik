e8274f321a4e7d12f03d05305f8f957ba28175c9
arifcalik <arifcalik@gmail.com>
2023-11-02 11:57:52 +0100

    Adding edge cases into unit tests

--- Nov01_python_intro2/test_unit_intro2.py
+++ Nov01_python_intro2/test_unit_intro2.py
@@ -6,20 +6,39 @@ class TestIntro2(unittest.TestCase):
         testList1 = [1, 1, 2, 0, 1, 3, 2, 1, 2, 1, 2, 4, 3, 1, 2,]
         result = arrayCheck(testList1)
         self.assertEqual(result, False)
+        #empty list
+        testList1 = []
+        result = arrayCheck(testList1)
+        #False is expected result
+        self.assertEqual(result, False)        
     
     def test_stringBits(self):
         input = 'Wwworldlldde'
         result = stringBits(input)
         self.assertEqual(result, 'Wwrdld')
+        #empty input!
+        input = ''
+        result = stringBits(input)
+        self.assertEqual(result, '')        
 
     def test_doubleChar(self):
         input = 'Hi-There'
         result = doubleChar(input)
         self.assertEqual(result, 'HHii--TThheerree')
+        # how about sending empty string
+        input = ''
+        result = doubleChar(input)
+        self.assertEqual(result, '')        
 
     def test_count_evens(self):
         input = [-1, 1, 7, 3, -123, 23, 67, 7,]
         result = count_evens(input)
+        self.assertEqual(result, 0)
+        '''
+        how about sending empty list!
+        '''
+        input = []
+        result = 0  #as expected
         self.assertEqual(result, 0)
         
 
